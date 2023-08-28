67fb76d4f9e11ed56e799641f092945e6d312cb2
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-28 15:35:22 +0530

    Add files via upload

---
+++ P04-JSX.jsx
@@ -0,0 +1,14 @@
+/*
+ Q- What is JSX in React ?
+
+ Ans.JSX stand for JavaScript XML.it allows us to write HTML inside 
+     javascript and place them into dom without using functions like
+     appendChild() or createElement(). */
+
+     const element = <h1>Hello, JSX!</h1>;
+
+     const element1 = <img src="image.jpg" alt="An example image" />;
+
+
+     
+
