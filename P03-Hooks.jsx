eab4c6a929230d355a34d3bbcb84e4bbdf52af2d
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-28 15:25:43 +0530

    Add files via upload

---
+++ P03-Hooks.jsx
@@ -0,0 +1,13 @@
+/*
+Q- What is Hooks ?
+
+
+
+Ans- React Hooks are a function type that allow you to hook
+     into react state and lifecycle features fom Functional components.
+     it cant used in class components. 
+
+    There are three types of Hooks in React 
+     ⦿ useState 
+     ⦿ useEffect   
+     ⦿ useContext      */
