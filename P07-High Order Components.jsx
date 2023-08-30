d9f5300aa9c3376e489edc93ac39cf4ebcd5d8ed
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-30 11:23:43 +0530

    Add files via upload

---
+++ P07-High Order Components.jsx
@@ -0,0 +1,6 @@
+/*
+
+Q-  What is High Order Components ?
+
+Ans.High Order Components is a Advance Technique for reusingn components 
+    logic .it is A Function that takes a components and return a new components.
