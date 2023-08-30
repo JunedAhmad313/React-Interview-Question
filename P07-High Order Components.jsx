da7803e4e3300325c48dbec24bcb56db044659a0
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-30 11:30:46 +0530

    Delete P07-High Order Components.jsx

--- P07-High Order Components.jsx
+++
@@ -1,6 +0,0 @@
-/*
-
-Q-  What is High Order Components ?
-
-Ans.High Order Components is a Advance Technique for reusingn components 
-    logic .it is A Function that takes a components and return a new components.
