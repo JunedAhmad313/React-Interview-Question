0bf5c0ab96ddff1636cbeb737d7baccda88154ba
Juned Ahmad <junedahmad9296@gmail.com>
2023-09-04 10:12:25 +0530

    Add files via upload

---
+++ P12-Lazy Loading.jsx
@@ -0,0 +1,6 @@
+/*
+Q. What is the purpose of using Lazy Loading ?
+
+Ans. Lazy Loading is a strategy to identify resource as non crictle.
+     this way can improve the Page Loading time without reducing the
+     page Size. */
